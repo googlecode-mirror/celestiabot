8ff1afe79d2d550bcfc1adebc3fe20334a7896d4
Roman Shishkin <admin@rom-soft.ru>
2012-11-24 22:32:00 +0300

    Rewrited dice (d.php)

--- modules/d.php
+++ modules/d.php
@@ -5,7 +5,34 @@ class d extends Module {
     public $params_no = true; 
     
     public function run($params, $type, $xmpp) {
-        return "1d".$params[1]." | ".rand(1, $params[1]);
+        if(count($params) == 2 && is_numeric($params[1])) {
+            if((int)$params[1] > 0 && (int)$params[1] < 1000) {
+                return "[".rand(1, $params[1])."]";
+            }else{
+                return "Число не входит в заданный диапазон 1-999!";
+            }
+            
+        }elseif(count($params) == 3 && is_numeric($params[1]) && is_numeric($params[2])) {
+            if((int)$params[1] > 0 && (int)$params[1] < 1000) {
+                if((int)$params[2] > 0 && (int)$params[2] < 1000) {
+                    $num = '';
+                    $sum = 0;
+                    for($i=1; $i<=$params[1]; $i++) {
+                        $r = rand(1, $params[2]);
+                        $num .= "[".$r."] ";
+                        $sum += $r;
+                    }
+                    
+                    return $num."| [".$sum."]";
+                }else{
+                    return "Второе число не входит в заданный диапазон 1-999!";
+                }
+            }else{
+                return "Первое число не входит в заданный диапазон 1-999!";
+            }
+        }else{
+             return "Количество параметров больше/меньше максимального или получено не-число!";
+        }
     }
 }
 ?>
